02a5af9a253da3c390961e404126c91e66c14b16
Raja Gopal <36278001+rajgopal2693@users.noreply.github.com>
2019-11-08 12:56:14 +0530

    Update SampleJobScript.sh

--- SampleJobScript.sh
+++ SampleJobScript.sh
@@ -1,12 +1,19 @@
 node{
 
-Project_ID = "${Project_ID}"
-Repo_Name = "${Repo_Name}"
+Project_ID = ${"Project_ID"}
+Repo_Name = ${"Repo_Name"}
+filepath=${"workspace"}
+cleanWs()
 
 stage("CheckOut Code")
 
 {
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rajgopal2693/maven.git']]])
 }
+stage("Maven Build")
+{
+env.WORKSPACE = pwd()
+mvn clean install -Dmaven.test.skip=true -o &>> $filepath
 
+}
 }
